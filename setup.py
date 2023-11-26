0cc3597fc742a23dba3842de4e97927d19bdc075
tony hollaar <thollaar@gmail.com>
2023-11-25 22:22:27 -0500

    updated github link

--- setup.py
+++ setup.py
@@ -6,18 +6,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='fiscal_calendar',
     version='0.1',
+    license='MIT',
+    description='Fiscal Retail Calendar - 4-5-4 week retail calendar',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tony Hollaar',
+    author_email='thollaar@gmail.com',
+    url='https://github.com/tonyhollaar/fiscal_calendar',
+    download_url='https://github.com/tonyhollaar/fiscal_calendar/archive/refs/tags/initial_release.tar.gz',
     packages=find_packages(),
     install_requires=[
         'pandas>=1.0.0',
         'numpy>=1.18.0',
     ],
-    url='https://github.com/tonyhollaar/fiscal_calendar',
-    license='MIT',
-    author='Tony Hollaar',
-    author_email='thollaar@gmail.com',
-    description='Fiscal Retail Calendar - 4-5-4 week retail calendar',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -26,5 +27,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
